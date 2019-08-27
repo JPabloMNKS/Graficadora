f82b3de550d434e902a7310a3a282c2dc3f69e5a
unknown <jpablo.bedoya7@gmail.com>
2019-08-26 20:53:57 -0400

    Base terminada

---
+++ Graficadora/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Graficadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox_Coordenada = new System.Windows.Forms.TextBox();
+            this.btnMaximize = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelTop.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panelTop.Controls.Add(this.btnMinimize);
+            this.panelTop.Controls.Add(this.btnMaximize);
+            this.panelTop.Controls.Add(this.btnClose);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(868, 59);
+            this.panelTop.TabIndex = 0;
+            this.panelTop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PanelTop_MouseDown);
+            this.panelTop.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PanelTop_MouseMove);
+            this.panelTop.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PanelTop_MouseUp);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Coral;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(793, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 59);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 59);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(868, 445);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(668, 445);
+            this.panel3.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(668, 445);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox_Coordenada);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(668, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 445);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(28, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sin(x)",
+            "Cos(x)",
+            "Tan(x)",
+            "x^2"});
+            this.comboBox1.Location = new System.Drawing.Point(28, 126);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 29);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Funciones";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // textBox_Coordenada
+            // 
+            this.textBox_Coordenada.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox_Coordenada.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Coordenada.Location = new System.Drawing.Point(98, 78);
+            this.textBox_Coordenada.Name = "textBox_Coordenada";
+            this.textBox_Coordenada.Size = new System.Drawing.Size(75, 21);
+            this.textBox_Coordenada.TabIndex = 0;
+            // 
+            // btnMaximize
+            // 
+            this.btnMaximize.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnMaximize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMaximize.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnMaximize.FlatAppearance.BorderSize = 0;
+            this.btnMaximize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGray;
+            this.btnMaximize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaximize.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaximize.ForeColor = System.Drawing.Color.White;
+            this.btnMaximize.Location = new System.Drawing.Point(718, 0);
+            this.btnMaximize.Name = "btnMaximize";
+            this.btnMaximize.Size = new System.Drawing.Size(75, 59);
+            this.btnMaximize.TabIndex = 4;
+            this.btnMaximize.Text = "𺊮";
+            this.btnMaximize.UseVisualStyleBackColor = false;
+            this.btnMaximize.Click += new System.EventHandler(this.BtnMaximize_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimize.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGray;
+            this.btnMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DimGray;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimize.ForeColor = System.Drawing.Color.White;
+            this.btnMinimize.Location = new System.Drawing.Point(643, 0);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(75, 59);
+            this.btnMinimize.TabIndex = 5;
+            this.btnMinimize.Text = "-";
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(24, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Valor:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 504);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelTop.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox_Coordenada;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnMaximize;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Graficadora/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graficadora
+{
+    public partial class Form1 : Form
+    {
+        // Drag Window Panel
+        private bool draggable;
+        private int mouseX;
+        private int mouseY;
+
+        // Variables de la aplicacion
+        Pen lapiz1 = new Pen(Color.DarkRed);
+        Pen lapiz2 = new Pen(Color.Green);
+
+        double[] valores = new double[20000];
+        double puntoX1 = 0;
+        double puntoX2 = 0;
+        double puntoY1 = 0;
+        double puntoY2 = 0;
+        int con = 0;
+
+        String funciones;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Drag Window
+        private void PanelTop_MouseDown(object sender, MouseEventArgs e)
+        {
+            draggable = true;
+            mouseX = Cursor.Position.X - this.Left;
+            mouseY = Cursor.Position.Y - this.Top;
+        }
+        private void PanelTop_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggable)
+            {
+                this.Left = Cursor.Position.X - mouseX;
+                this.Top = Cursor.Position.Y - mouseY;
+            }
+        }
+        private void PanelTop_MouseUp(object sender, MouseEventArgs e)
+        {
+            draggable = false;
+        }
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void BtnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        // Application
+
+        private void GraficarFunciones()
+        {
+            Graphics dibujo = this.pictureBox1.CreateGraphics();
+            int xcentro = pictureBox1.Width / 2;
+            int ycentro = pictureBox1.Height / 2;
+
+            dibujo.TranslateTransform(xcentro, ycentro);
+            dibujo.ScaleTransform(1, -1);
+            if(textBox_Coordenada.Text == "")
+            {
+                MessageBox.Show("Ingrese un valor", "Valor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                for(int i = -xcentro; i< xcentro; i += 8)
+                {
+                    dibujo.DrawLine(lapiz1, 5, i, -5, i);
+                    dibujo.DrawLine(lapiz1, i, 5, i, -5);
+                }
+                funciones = comboBox1.Text;
+                if (funciones == "Funciones")
+                {
+                    MessageBox.Show("Seleccione una funcion", "Funciones", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    con = 0;
+                    for(double x = xcentro * -1; x < xcentro * 2; x += 0.1)
+                    {
+                        switch (funciones)
+                        {
+                            case "Cos(x)":
+                                valores[con] = Math.Cos(x);
+                                break;
+                            case "Sin(x)":
+                                valores[con] = Math.Sin(x);
+                                break;
+                            case "Tan(x)":
+                                valores[con] = Math.Tan(x);
+                                break;
+                            case "x^2":
+                                valores[con] = Math.Pow(x, 2);
+                                break;
+                        }
+                        con++;
+                    }
+                    con = 1;
+                    for(double xx = xcentro * -1 + 0.1; xx < xcentro * 2; xx += 0.1)
+                    {
+                        // sacamos coordenadas 1
+                        puntoX1 = (xx - 0.1) * (pictureBox1.Width / (Convert.ToInt32(textBox_Coordenada.Text) * 2));
+                        puntoY1 = valores[con - 1] * ycentro;
+
+                        // sacamos coordenadas 2
+                        puntoX2 = xx * (pictureBox1.Width / (Convert.ToInt32(textBox_Coordenada.Text) * 2));
+                        puntoY2 = valores[con] * ycentro;
+                        dibujo.DrawLine(lapiz2, Convert.ToSingle(puntoX1), Convert.ToSingle(puntoY1), Convert.ToSingle(puntoX2), Convert.ToSingle(puntoY2));
+                        con++;
+                    }
+                }
+
+            }
+        }
+
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int xcentro = pictureBox1.Width / 2;
+            int ycentro = pictureBox1.Height / 2;
+            e.Graphics.TranslateTransform(xcentro, ycentro);
+            e.Graphics.ScaleTransform(1, -1);
+
+            Point p1 = new Point(xcentro*-1, xcentro*2);
+
+            e.Graphics.DrawLine(lapiz1, xcentro * -1, 0, xcentro * 2, 0); // Eje X
+            e.Graphics.DrawLine(lapiz1, 0, ycentro, 0, ycentro * -1);
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            funciones = comboBox1.Text;
+            pictureBox1.Image = null;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            GraficarFunciones();
+        }
+
+        private void BtnMaximize_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                pictureBox1.Image = null;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                pictureBox1.Image = null;
+            }
+        }
+    }
+}
